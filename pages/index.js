bbdf80b1b7f134f34bd4e312369125f7e19645a8
Daniel Muzike <danymuziki90@gmail.com>
2023-08-16 17:04:32 +0200

    channges mongodb

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,17 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Home() {
+    const { data: session } = useSession()
+  if(!session){
     return ( 
-    <div className="bg-blue-900 w-screen flex items-center">
-        <div className='text-center w-full'>
-            <button className='bg-white p-2 px-4 rounded-lg'>Login</button>
+        <div className="bg-blue-900 h-screen flex items-center">
+            <div className='text-center w-full'>
+                <button onClick={() => signIn('google')}  className='bg-white p-2 px-4 rounded-lg'>Login </button>
+            </div>
         </div>
-    </div>
-        )
+            );
+  }
+    return (
+        <div>Logged In {session.user.email}</div>
+    );
     }
